021b44c84f31800185cf9bdc1714d4b475e03c89
claralonguet <151842131+claralonguet@users.noreply.github.com>
2025-05-10 19:28:08 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/culinar/MainActivity.kt
+++ app/src/main/java/com/example/culinar/MainActivity.kt
@@ -3,45 +3,25 @@ package com.example.culinar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import com.example.culinar.LoginScreen
+import com.example.culinar.SignupScreen
+import com.example.culinar.ProfileScreen
 import com.example.culinar.ui.theme.CulinarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CulinarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                var currentScreen by remember { mutableStateOf("login") }
+
+                when (currentScreen) {
+                    "login" -> LoginScreen { currentScreen = "signup" }
+                    "signup" -> SignupScreen { currentScreen = "profile" }
+                    "profile" -> ProfileScreen { currentScreen = "login" }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CulinarTheme {
-        Greeting("Android")
     }
 }
